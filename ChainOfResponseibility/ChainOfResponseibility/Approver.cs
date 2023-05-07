dc780de2b258274d083c594bcf93b5961807d2ab
tushar087 <tushar087@gmail.com>
2023-05-08 00:14:12 +0530

    Chain of Responseibility

---
+++ ChainOfResponseibility/ChainOfResponseibility/Approver.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ChainOfResponseAbility
+{
+    public abstract class Approver
+    {
+        protected Approver? _successor;
+
+        public void SetSucessor(Approver? sucessor)
+        {
+            _successor = sucessor;
+        }
+
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+}
