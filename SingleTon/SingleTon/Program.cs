417c95c8c93118a7dba12cd73be3399ae650a309
tushar087 <tushar087@gmail.com>
2023-06-22 00:16:12 +0530

    Singleton Design Pattern

---
+++ SingleTon/SingleTon/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Parallel.Invoke(
+    () => { SingleTon.SingleTon ton = SingleTon.SingleTon.GetInstance(); ton.PrintDetails("From Teacher"); });
+
+
+Parallel.Invoke(
+    () => { SingleTon.SingleTon ton = SingleTon.SingleTon.GetInstance(); ton.PrintDetails("From Student"); });
+
+Console.ReadLine();
---
+++ SingleTon/SingleTon/SingleTon.cs
@@ -0,0 +1,39 @@
+﻿
+namespace SingleTon
+{
+    public sealed  class SingleTon
+    {
+        private static int Counter = 0;
+
+        private static SingleTon Instance = null;
+
+        private static readonly object Instancelock = new object();
+
+        public static SingleTon GetInstance()
+        {
+            if (Instance == null)
+            { 
+
+            lock (Instancelock)
+            {
+                if (Instance == null)
+                    Instance = new SingleTon();
+            }
+
+           }
+            return Instance;
+        }
+
+        private SingleTon()
+        {
+            Counter++;
+            Console.WriteLine("Counter Value " + Counter.ToString());
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
