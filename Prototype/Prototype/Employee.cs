4168800104e2fc50323b3fecc310da92bbb96b98
tushar087 <tushar087@gmail.com>
2023-06-28 23:39:57 +0530

    Prototype Design Pattern

---
+++ Prototype/Prototype/Employee.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace Prototype
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        public Address EmpAddress { get; set; }
+
+        public Employee GetShallowCopy()
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+
+        public Employee GetDeepCopy()
+        {
+            Employee employee = (Employee)this.MemberwiseClone();
+
+            employee.EmpAddress = EmpAddress.GetClone();
+            return employee;
+        }
+    }
+
+    public class Address
+    {
+        public string address { get; set; }
+
+        public Address GetClone()
+        {
+            return (Address)this.MemberwiseClone();
+        }
+    }
+}
---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,32 @@
+﻿using Prototype;
+
+Employee emp1 = new Employee
+{
+    Name = "Tushar",
+    Department = "IT",
+    EmpAddress = new Address() { address = "Jodhpur" }
+};
+
+Employee emp2=emp1.GetShallowCopy();
+
+emp2.Name = "Kuldeep";
+
+emp2.EmpAddress.address = "Chandigarh";
+
+Employee emp3 = emp2.GetDeepCopy();
+
+emp3.EmpAddress.address = "Jaipur";
+
+Console.WriteLine("Employee 1");
+
+Console.WriteLine("Name: " + emp1.Name + ", Address: "  + emp1.EmpAddress.address + ", Dept: " +emp1.Department);
+
+Console.WriteLine("Employee 2 created through Shallow Copy");
+
+Console.WriteLine("Name: " + emp2.Name + ", Address: " + emp2.EmpAddress.address + ", Dept: " + emp2.Department);
+
+Console.WriteLine("Employee 3 created through Deep Copy");
+
+Console.WriteLine("Name: " + emp3.Name + ", Address: " + emp3.EmpAddress.address + ", Dept: " + emp3.Department);
+
+Console.ReadLine();
